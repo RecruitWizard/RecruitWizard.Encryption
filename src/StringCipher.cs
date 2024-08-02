d1ff63ab4aac2fdea39a8c0c5d9df218841c5615
Ben Macpherson <ben@recruitwizard.com>
2024-08-02 15:40:34 +1000

    Use Bouncy Castle for .net Core compatibility

--- src/StringCipher.cs
+++ src/StringCipher.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace RecruitWizard.Encryption
 {
@@ -25,38 +28,27 @@ namespace RecruitWizard.Encryption
             using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
-                {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-                    using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
-                    {
-                        using (var memoryStream = new MemoryStream())
-                        {
-                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
-                            {
-                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                                cryptoStream.FlushFinalBlock();
-                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-                                var cipherTextBytes = saltStringBytes;
-                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Convert.ToBase64String(cipherTextBytes);
-                            }
-                        }
-                    }
-                }
+                var engine = new RijndaelEngine(256);
+                var blockCipher = new CbcBlockCipher(engine);
+                var cipher = new PaddedBufferedBlockCipher(blockCipher, new Pkcs7Padding());
+                var keyParam = new KeyParameter(keyBytes);
+                var keyParamWithIV = new ParametersWithIV(keyParam, ivStringBytes, 0, 32);
+
+                cipher.Init(true, keyParamWithIV);
+                var comparisonBytes = new byte[cipher.GetOutputSize(plainTextBytes.Length)];
+                var length = cipher.ProcessBytes(plainTextBytes, comparisonBytes, 0);
+
+                cipher.DoFinal(comparisonBytes, length);
+                //                return Convert.ToBase64String(comparisonBytes);
+                return Convert.ToBase64String(saltStringBytes.Concat(ivStringBytes).Concat(comparisonBytes).ToArray());
             }
         }
 
-        public static string Decrypt(string encryptedText, string passPhrase)
+        public static string Decrypt(string cipherText, string passPhrase)
         {
             // Get the complete stream of bytes that represent:
             // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
-            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(encryptedText);
+            var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
             // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
             var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
             // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
@@ -67,23 +59,28 @@ namespace RecruitWizard.Encryption
             using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
             {
                 var keyBytes = password.GetBytes(Keysize / 8);
-                using (var symmetricKey = new RijndaelManaged())
-                {
-                    symmetricKey.BlockSize = 256;
-                    symmetricKey.Mode = CipherMode.CBC;
-                    symmetricKey.Padding = PaddingMode.PKCS7;
-                    using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
-                    {
-                        using (var memoryStream = new MemoryStream(cipherTextBytes))
-                        {
-                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
-                            using (var streamReader = new StreamReader(cryptoStream, Encoding.UTF8))
-                            {
-                                return streamReader.ReadToEnd();
-                            }
-                        }
-                    }
-                }
+                var engine = new RijndaelEngine(256);
+                var blockCipher = new CbcBlockCipher(engine);
+                var cipher = new PaddedBufferedBlockCipher(blockCipher, new Pkcs7Padding());
+                var keyParam = new KeyParameter(keyBytes);
+                var keyParamWithIV = new ParametersWithIV(keyParam, ivStringBytes, 0, 32);
+
+                cipher.Init(false, keyParamWithIV);
+                var comparisonBytes = new byte[cipher.GetOutputSize(cipherTextBytes.Length)];
+                var length = cipher.ProcessBytes(cipherTextBytes, comparisonBytes, 0);
+
+                cipher.DoFinal(comparisonBytes, length);
+                //return Convert.ToBase64String(saltStringBytes.Concat(ivStringBytes).Concat(comparisonBytes).ToArray());
+
+                var nullIndex = comparisonBytes.Length - 1;
+                while (comparisonBytes[nullIndex] == (byte)0)
+                    nullIndex--;
+                comparisonBytes = comparisonBytes.Take(nullIndex + 1).ToArray();
+
+
+                var result = Encoding.UTF8.GetString(comparisonBytes, 0, comparisonBytes.Length);
+
+                return result;
             }
         }
 
